2eb87aeaa0d2304fdddc6b54ba25ae0c059f18fa
eyob42 <eyobadigeh07@gmail.com>
2025-11-01 22:43:26 +0300

    feat: initialize Django project and customize admin for minimal user creation

---
+++ dwitter/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    #only display username field
+    fields = ["username"]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
