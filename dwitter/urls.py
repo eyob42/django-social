28f9f7d2f661812c62ed486ca5fbfb670f2d4880
eyob42 <eyobadigeh07@gmail.com>
2025-11-02 03:47:28 +0300

    Add individual profile pages with dynamic linking between profiles

--- dwitter/urls.py
+++ dwitter/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import dashboard, profile_list
+from .views import dashboard, profile_list, profile
 
 
 app_name = "dwitter"
@@ -7,4 +7,5 @@ app_name = "dwitter"
 urlpatterns = [
     path("",dashboard, name="dashboard"),
     path("profile_list/",profile_list, name="profile_list"),
+    path("profile/<int:pk>",profile, name="profile"),
 ]
