2e47e24f3c5b7edcb79144a58c682ec3c60d268e
eyob42 <eyobadigeh07@gmail.com>
2025-11-02 22:36:06 +0300

    Add follow/unfollow buttons and backend logic in profile view

--- dwitter/views.py
+++ dwitter/views.py
@@ -11,5 +11,17 @@ def profile_list(request):
 
 
 def profile(request, pk):
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
     profile = Profile.objects.get(pk=pk)
+    if request.method == "POST":
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile)
+        elif action == "unfollow":
+            current_user_profile.profile.remove(profile)
+        current_user_profile.save()
     return render(request, "dwitter/profile.html",{"profile":profile})
